8a47b38fc680c5feee81e472972a34e1a7cea29c
Nhan Nguyen <nhnguyen@ualberta.ca>
2023-02-09 23:47:23 -0700

    remove debug

--- rust/sum_of_interval.rs
+++ rust/sum_of_interval.rs
@@ -4,34 +4,19 @@ fn process(mut vec: Vec<(i32,i32)>) -> Vec<(i32,i32)> {
     vec.dedup();
     vec.sort();
     
-    for interval in &vec {
-        println!("{:?}",interval);
-    }
-    println!("hi");
-    
     for i in 0..vec.len() {
         let mut interval = vec[i].clone();
         
         for j in i..vec.len() {
             let interval2 = &vec[j];
 
-//             println!("C: {:?} | {:?} ", interval2, interval);
             if i != j && (interval.1 > interval2.0) {
-                println!("B: {:?} | {:?}",interval, interval2 );
-
                 interval.0 = if interval.0 <= interval2.0 { interval.0} else {interval2.0};
                 interval.1 = if interval.1 <= interval2.1 { interval2.0} else {interval.0};
-                
-                println!("A: {:?} | {:?}", interval, interval2);
             }
         }
         intervals.push(interval);
     }
-
-    for interval in &intervals {
-        println!("{:?}",interval);
-    }
-    println!("done");
 
     intervals    
 }
